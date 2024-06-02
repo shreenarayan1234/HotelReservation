90d82ffb6307523386cf71651217cdd8da24b831
shreenarayan1234 <shreenarayanshrestha434@gmail.com>
2024-06-02 16:06:07 +0545

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+route::get('/',[AdminController::class,'home']);
+
+
+route::get('/home',[AdminController::class,'index'])->name('home');
