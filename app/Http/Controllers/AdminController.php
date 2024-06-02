26c4d55d54b764bef78b7b7bac139695850eb928
shreenarayan1234 <shreenarayanshrestha434@gmail.com>
2024-06-02 17:44:15 +0545

    adding data from admin panel to DB

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Room;
 
 class AdminController extends Controller
 {
@@ -31,5 +32,39 @@ class AdminController extends Controller
 
         public function home(){
             return view('home.index');
+        }
+
+        public function create_room(){
+            return view('admin.create_room');
+        }
+
+        public function add_room(Request $request){
+            
+            $data = new Room;
+
+            $data->room_title = $request->title;  //Here room_title is a field_name in DB and title is name of form
+            $data->description = $request->description;
+
+            $data->price = $request->price;
+
+            $data->wifi = $request->wifi;
+
+            $data->room_type = $request->type;
+
+            $image = $request->image;  //get image from form
+
+            if($image){
+
+                $imagename = time().'.'.$image->getClientOriginalExtension();  //modify the image name using time function
+
+                $request->image->move('room',$imagename);  //moving the image in public folder with certain name
+
+                $data->image = $imagename;   //storing the data in DB
+            }
+
+            $data->save();
+
+            return redirect()->back();
+
         }
 }
